ee2184371a3d431b1adf9f53f06a7c031e8fc3b1
Lapid P. <bonk.lapid@gmail.com>
2025-09-16 15:26:53 +0100

    app version 0.1.0
    - add basic api including status, ask, and clear endpoints

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# ArcFusion-Assignment
-Chat-with-PDFs backend
+How to run:
+
+1. Open command prompt at the working repository root. In command prompt, run
+- pip install -r requirement.txt
+- uvicorn src.api:app --reload
+
+2. Open http://127.0.0.1:8000/docs to see an interactive API page to manually test the functionality
---
+++ src/api.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+# The object that represents the web service
+app = FastAPI(title="Chat-With-PDFs", version="0.1.0", description="Initial API")
+
+# This class define a shape for when client sends user question and initialize session_id
+class Ask(BaseModel):
+    session_id: str
+    question: str
+
+# This class helps clear the session using session_id
+class Clear(BaseModel):
+    session_id: str
+
+# For checking server status
+@app.get("/status")
+def status():
+    return {"status": "ok"}
+
+# For asking question (we will implement RAG + LLM later in the next version)
+@app.post("/ask")
+def ask(body: Ask):
+    raise HTTPException(status_code=501) # Using 501 just as a placeholder for testing for now
+
+# For showing that the session memory has been cleared
+@app.post("/clear")
+def clear(body: Clear):
+    return {"cleared": True}
